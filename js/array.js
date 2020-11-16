c27329821f19c2d62ce38c3bd9a212222f35c10c
ESinkev <73878808+ESinkev@users.noreply.github.com>
2020-11-17 01:00:34 +0200

    sarasai atnaujinta

--- js/array.js
+++ js/array.js
@@ -1,31 +1,35 @@
 /*
 Saraso tipo kintamieji:
 sinonimai: sarasas, masyvas, array, matrica*
-inicijuojama su lauztiniais skliausteliais;
+inicijuojama su lauztiniais skliausteliais; - skaicius, textas, ...
 viduje gali buti ivairiu tipu reiksmiu, bet rekomenduotina, kad butu tik vieno tipo reiksmes.
 */
 
 // Pazymiai 
+// petriukoPazymiai = 2, 10, 7, 5
 
-const petriukoPazymiai = [2, 10, 7, 5];
+const petriukoPazymiai = [2, 10, 7, 5, 10];
 console.log(petriukoPazymiai);
 
 // Kaip suskaiciuoti pazymiu vidurki?
+
+
 let suma = 0;
 suma = suma + petriukoPazymiai[0];
 suma = suma + petriukoPazymiai[1];
 suma = suma + petriukoPazymiai[2];
 suma = suma + petriukoPazymiai[3];
-// suma = suma + petriukoPazymiai[4];
+suma = suma + petriukoPazymiai[4];
 
 const kiekis = petriukoPazymiai.length;
 const vidurkis = suma / kiekis;
 
 console.log(vidurkis);
 
-const miestai = ['Vilnius', 'Kaunas', 'Klaipeda', 'Anyksciai'];
+const miestai = ['Vilnius', 'Kaunas', 'Klaipeda', 'Anyksciai', 'Druskininkai'];
 console.log(miestai);
 
+console.log(miestai[4]);
 console.log(miestai[3]);
 console.log(miestai[2]);
 console.log(miestai[1]);
