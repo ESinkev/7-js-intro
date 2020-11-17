12e7aaf3898846235ebc95c6ea496783eafc3295
ESinkev <73878808+ESinkev@users.noreply.github.com>
2020-11-17 17:11:46 +0200

    pick letter galutinis update

--- js/pickLetters.js
+++ js/pickLetters.js
@@ -1,53 +1,61 @@
-function pickLetters() {
-
-// input validation
-if (typeof text !== 'string') {
-    return 'ERROR: pirmoji reiksme ne teksto tipo.';
-}
-if (typeof step !== 'number') {
-    return 'ERROR: antroji reiksme ne skaiciaus tipo.';
-}
-const size = text.length;
-
-if (step > size) {
-    return 'ERROR: zingsnis yra per didelis lyginant su teksto ilgiu';
-}
-if (step === 0) {
-    return 'ERROR: zingsnis negali buti nulinis';
-}
-// logic
-let rez = '';
-
-for (let i = step - 1; i < size; i+=step) {
-   // if (i % step === step -1) {
-        rez += text[i];
+function pickLetters(text, step) {
+    // input validation
+    if (typeof text !== 'string') {
+        return 'ERROR: pirmoji reiksme ne teksto tipo.';
     }
+    if (text === '') {
+        return 'ERROR: tekstas negali buti tuscias.';
+    }
+    if (typeof step !== 'number') {
+        return 'ERROR: antroji reiksme ne skaiciaus tipo.';
+    }
+    if (step % 1 !== 0) {
+        return 'ERROR: zingsnis turi buti sveikasis skaicius.'
+    }
+
+    const size = text.length;
+
+    if (step > size) {
+        return 'ERROR: zingsnis yra per didelis lyginant su teksto ilgiu.'
+    }
+    if (step === 0) {
+        return 'ERROR: zingsnis negali buti nulinis.';
+    }
+
+    // logic
+    let rez = '';
+
+    if (step > 0) {
+        const firstLetterPosition = step - 1;
+        for (let i = firstLetterPosition; i < size; i += step) {
+            rez += text[i];
+        }
+    } else {
+        const firstLetterPosition = size + step;
+        for (let i = firstLetterPosition; i >= 0; i += step) {
+            rez += text[i];
+        }
+    }
+
+    // post logic validation
+
+    // return
+    return rez;
 }
 
-// post logic validation
+console.log(pickLetters(1561, 2));
+console.log(pickLetters('1561', '2'));
+console.log(pickLetters('abc', 4));
+console.log(pickLetters('abc', 0));
+console.log(pickLetters('abc', 1.5), '->', 'c');
+console.log(pickLetters('abcdefghijkl', 1.5), '->', 'ci');
 
-// return
-return rez;
-
-}
-
+console.log(pickLetters('abc', 3), '->', 'c');
 console.log(pickLetters('abcdefg', 2), '->', 'bdf');
-console.log(pickLetters('abcdefghijkl', 3 ), '->', 'cfil' );
+console.log(pickLetters('abcdefghijkl', 3), '->', 'cfil');
 
-console.log(pickLetters( 'abc', 0 ));
-console.log(pickLetters( 'abc', 4 ) );
-console.log(pickLetters( 1561, 2 ) );
-console.log(pickLetters( '1561', 2 ) );
+console.log(pickLetters('abc', -3), '->', 'a');
+console.log(pickLetters('abcdefg', -2), '->', 'fdb');
+console.log(pickLetters('abcdefghijkl', -3), '->', 'jgda');
 
-
-/*
-
-step: 3
-
-_ _ _ _
-abcdefgijkl  -> cfil
---------
-
-
-
-*/
+console.log(pickLetters('', 3));
